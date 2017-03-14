eb09fc9f6d63b81571ad4f0a65155457937a50ed
daniel kwangho Suk <khseok1060@gmail.com>
2017-03-14 17:51:29 +0900

    express middleware test

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 const app = express();
 
+function logger (req, res, next) {
+    console.log("I'm Logger");
+    next();
+}
+
+app.use(logger);
+
 app.listen(3000, function(){
     console.log('Server is listening')
 })
