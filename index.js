f233dbea0aba8bedf807aafb7463bc1fc1e60247
daniel kwangho Suk <khseok1060@gmail.com>
2017-03-14 17:46:45 +0900

    Express Application 만들기

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const http = require('http');
+import express from 'express';
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World\n');
-    } else if(req.url === '/users') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('User List\n');
-    } else {
-        res.statusCode = 404;
-        res.end('Not Found\n');
-    }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, function(){
+    console.log('Server is listening')
+})
